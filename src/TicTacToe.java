cf9f4f7e56191475c80b0a2b838da1cabeee6579
Progress Nemaranzhe <140755656+KP-Nemaranzhe@users.noreply.github.com>
2025-02-12 16:20:00 +0200

    game verify winneer

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,7 @@
 // first we have to import the essential tools we will need for development
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.*;
 
 // Create the game class
@@ -11,15 +13,29 @@ public class TicTacToe{
     //Create the game window
     JFrame frame = new JFrame();
 
+    //Add Panel for the text
+    JLabel textLabel = new JLabel(); //text
+    JPanel textPanel = new JPanel();
+
     //constructor
     public TicTacToe() {
+        //Game Window
+
         frame.setVisible(true); //make frame visible
         frame.setSize(boardWidth,boardHeight); // this will set the size of our window
         frame.setLocationRelativeTo(null); //this will open our window at the center of the screen
         frame.setResizable(false); //The user should not be able to resize the window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //For user to close the game
         frame.setLayout(new BorderLayout());
-        
+
+        //TEXT LABEL
+        textLabel.setBackground(Color.darkGray); //this set the background of the label
+        textLabel.setForeground(Color.white); //change the text color to white
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER); //This will center the text instead of starting from left
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
     }
     
 }
