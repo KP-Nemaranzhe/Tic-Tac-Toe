636e2445ee37d88b53a3ee4a8f683f67c875403a
Progress Nemaranzhe <140755656+KP-Nemaranzhe@users.noreply.github.com>
2025-02-12 14:43:14 +0200

    Created game window

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App{
+    // main method
+    public static void main(String[] args) throws Exception{
+        // Create the tic-tac-toe object 
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,25 @@
+// first we have to import the essential tools we will need for development
+import java.awt.BorderLayout;
+import javax.swing.*;
+
+// Create the game class
+public class TicTacToe{
+    //define the viriable for the size of the game window
+    int boardWidth = 600;
+    int boardHeight = 650; //50px will be for the text panel on top
+    
+    //Create the game window
+    JFrame frame = new JFrame();
+
+    //constructor
+    public TicTacToe() {
+        frame.setVisible(true); //make frame visible
+        frame.setSize(boardWidth,boardHeight); // this will set the size of our window
+        frame.setLocationRelativeTo(null); //this will open our window at the center of the screen
+        frame.setResizable(false); //The user should not be able to resize the window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //For user to close the game
+        frame.setLayout(new BorderLayout());
+        
+    }
+    
+}
